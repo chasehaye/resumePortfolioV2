5cca8fe396c7453741bd0b6392996582f9c9192f
chasehaye <chasehayejoc@gmail.com>
2023-11-01 14:28:24 -0700

    base structure complete

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-window.scroll({
-    top: 100,
-    left: 100,
-    behavior: "smooth",
-  });
+// Elements
+const navBarToggleBtn = document.getElementById('menuBtn');
+const navBar = document.querySelector('.nav_text');
+const modal = document.querySelector(".modal");
+const btn = document.getElementById("modalBtn");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+// NavBar toggle
+function toggleNav(){
+    if(navBar.style.visibility === "visible"){
+        navBar.style.visibility = "hidden"
+      }else{
+        navBar.style.visibility = "visible"
+      }
+
+}
+
+navBarToggleBtn.addEventListener('click', toggleNav)
+
+// Modal toggle
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(e) {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+}
